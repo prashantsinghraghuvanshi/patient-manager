d0d83c5054a785306cfe8db2bb4444d61e455017
Prashant Singh <singhprashant0314@gmail.com>
2025-06-14 19:57:35 +0530

    feat(infra): create VPC in LocalStack using AWS CLI

---
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -0,0 +1,32 @@
+package com.pm.stack;
+
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
+
+public class LocalStack extends Stack {
+    private final Vpc vpc;
+
+    public LocalStack(final App scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder
+                .create(this, "PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
+    }
+
+    public static void main(final String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+        StackProps props = StackProps.builder()
+                .synthesizer(new BootstraplessSynthesizer())
+                .build();
+
+        new LocalStack(app, "localstack", props);
+        app.synth();
+        System.out.println("App synthesizing in progress...");
+    }
+}
